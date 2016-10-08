09b6722b81eb3edd8ce618a428505a581943b7ec
Robson Gian Perassoli <robsonperassoli@gmail.com>
2016-10-08 15:06:46 -0300

    add database support in heroku

--- src/api/db.clj
+++ src/api/db.clj
@@ -1,4 +1,14 @@
 (ns api.db
-  (:require [monger.core :as mg]))
+  (:require [monger.core :as mg]
+            [environ.core :refer [env]]))
 
-(def db (mg/get-db (mg/connect) "soccer-mgr"))
+
+(defn get-database-uri []
+  (or (env :database-uri) "mongodb://127.0.0.1/matches-api"))
+
+(defn get-db []
+  (let [uri (get-database-uri)
+        {:keys [conn db]} (mg/connect-via-uri uri)]
+        db))
+
+(def db (get-db))
