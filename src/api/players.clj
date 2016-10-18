253f222bf24da26a418aea8e10ed0fd64f9adc18
Robson Gian Perassoli <robsonperassoli@gmail.com>
2016-10-18 09:56:33 -0200

    Validations on players resource and teams file updated to have the base64 image instead of the url

--- src/api/players.clj
+++ src/api/players.clj
@@ -3,19 +3,34 @@
             [api.util :refer [identify stringfy-id stringfy-ids new-object-id is-valid-id?]]
             [api.db :refer [db]]))
 
+(def invalid-player-message {:message "Could not save the player due do invalid information supplied"})
+
 (defn list []
   (-> (mc/find-maps db :players) stringfy-ids))
 
+(defn is-valid? [player]
+  (let [name (:name player)]
+    (and
+      (not (empty? name))
+      (nil? (:id (get-by-name name))))))
+
 (defn post [player]
-    (-> (mc/insert-and-return db :players (identify player)) stringfy-id))
+  (if (is-valid? player)
+    (-> (mc/insert-and-return db :players (identify player)) stringfy-id)
+    invalid-player-message))
 
 (defn get-by-id [id]
   (when (is-valid-id? id)
         (-> (mc/find-map-by-id db :players (new-object-id id)) stringfy-id)))
 
+(defn get-by-name [name]
+  (-> (mc/find-one-as-map db :players {:name name}) stringfy-id))
+
 (defn put [id player]
-  (mc/update-by-id db :players (new-object-id id) player)
-  (get-by-id id))
+  (if (is-valid? player)
+    (do (mc/update-by-id db :players (new-object-id id) player)
+        (get-by-id id))
+    invalid-player-message))
 
 (defn delete [id]
   (mc/remove-by-id db :players (new-object-id id)))
